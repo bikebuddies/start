6e08285082f149d42f5346b98b92d442b0485b26
LauraMap <laura.saxer@student.uibk.ac.at>
2023-06-13 12:08:13 +0200

    L: Marker für Hauptstädte eingefügt

--- main.js
+++ main.js
@@ -1,34 +1,57 @@
-/* Code inspiriert von Vienna mobile Beispiel */
+/* Code von Vienna mobile Beispiel */
 
 // Zentrum Karte Objekt
-let eisenstadt = {
-    lat: 47.845556,
-    lng: 16.518889,
-    title: "Eisenstadt, Burgenland"
+let stpolten = {
+    lat: 48.18735,
+    lng: 15.64139,
+    title: "St. Pölten, Niederösterreich"
 }
 
-// Karte initialisieren
-let map = L.map("map").setView([
-    eisenstadt.lat, eisenstadt.lng
-], 7);
-
-//Fullscreen control
-map.addControl(new L.Control.Fullscreen());
+// Karte initialisieren und Fullscreen Control 
+let map = L.map("map", {
+    fullscreenControl: true
+}).setView([
+    stpolten.lat, stpolten.lng
+], 7.5);
 
 // Hintergrundlayer 
 //!Können wir noch schauen, welche besser passen!
 let layerControl = L.control.layers({
     "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.DE").addTo(map),
     "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
-    "CycleTrails": L.tileLayer.provider("WaymarkedTrails.cycling"),
-    /*"BasemapAT Grau": L.tileLayer.provider("BasemapAT.grau").addTo(map),
-    "BasemapAT Standard": L.tileLayer.provider("BasemapAT.basemap"),
-    "BasemapAT High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
-    "BasemapAT Gelände": L.tileLayer.provider("BasemapAT.terrain"),
-    "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
-    "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
-    "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay")*/
+    "CycleTrails": L.tileLayer.provider("CyclOSM"),
 }).addTo(map);
+
+// Marker Hauptstädte
+const STAEDTE = [
+    {
+        title: "St. Pölten, Niederösterreich",
+        lat: 48.18735,
+        lng: 15.64139,
+        wikipedia: "https://de.wikipedia.org/wiki/St._P%C3%B6lten"
+    },
+    {
+        title: "Wien, Wien",
+        lat: 48.208174,
+        lng: 16.373819,
+        wikipedia: "https://de.wikipedia.org/wiki/Wien"
+    },
+    {
+        title: "Eisenstadt, Burgenland",
+        lat: 47.845556,
+        lng: 16.518889,
+        wikipedia: "https://de.wikipedia.org/wiki/Eisenstadt"
+    },
+]
+
+for (let stadt of STAEDTE) {
+    //Marker für den Stopp
+    let marker = L.marker([stadt.lat, stadt.lng])
+        .addTo(map)
+        .bindPopup(`${stadt.title} <br>
+    <a href="${stop.wikipedia}">Wikipedia</a>
+    `)
+    };
 
 /*
 // Marker Stephansdom
